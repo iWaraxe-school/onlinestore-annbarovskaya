9bda4327dabd24bd1f901bc7bf043dde0f32f9b2
annbarovskaya <annbarovskaya@gmail.com>
2022-12-20 11:04:02 +0300

    Added StoreApp class and reflections

---
+++ consoleApp/src/main/java/by/issoft/StoreApp.java
@@ -0,0 +1,17 @@
+package by.issoft;
+
+import module3.oop.Product;
+
+import java.lang.reflect.Method;
+import java.math.BigDecimal;
+import java.util.Arrays;
+
+public class StoreApp {
+    public static void main (String[] args) {
+        Product product = new product ('name':" ", 'rate': "", 'price': "" );
+        final class productClass = Product.class;
+        final String methodName = 'ProductMethod';
+        final Method method = productClass.getDeclaredMethod(methodName, String.product);
+
+    }
+}

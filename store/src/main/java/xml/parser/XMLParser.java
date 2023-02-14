6ab06ab8d22bb9072816e75ea412acde4d09c9aa
annbarovskaya <annbarovskaya@gmail.com>
2023-02-14 12:27:06 +0300

    Added map to xml parser, fixed other files issues

--- store/src/main/java/xml/parser/XMLParser.java
+++ store/src/main/java/xml/parser/XMLParser.java
@@ -11,35 +11,45 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class XMLParser {
-    public static void main(String[] args) {
+    public static Map<String, String> xmlHandler() {
+        Map<String, String> configMap = new LinkedHashMap<String, String>();
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder = null;
+        Document doc = null;
         try {
             File inputFile = new File("config.xml");
-            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-            Document doc = dBuilder.parse(inputFile);
-            doc.getDocumentElement().normalize();
-            NodeList nList = doc.getElementsByTagName("sort");
-            Node sortTag = nList.item(0);
-            NodeList childNodes = sortTag.getChildNodes();
-            for (int i = 0; i < childNodes.getLength(); i++) {
-                Node node = childNodes.item(i);
-                System.out.println("Current Element:" + node.getNodeName());
-                if (node.getNodeType() == Node.ELEMENT_NODE) {
-                    Element eElement = (Element) node;
-                    String tagName = eElement.getTagName();
-                    String textContent = eElement.getTextContent();
-                    System.out.println("Name:" + tagName + "Tag" + textContent );
-                }
-            }
-        } catch (ParserConfigurationException pce) {
+            dBuilder = dbFactory.newDocumentBuilder();
+            doc = dBuilder.parse(inputFile);
+
+        } catch (
+                ParserConfigurationException pce) {
             pce.printStackTrace();
-        } catch (SAXException ex) {
+        } catch (
+                SAXException ex) {
             ex.printStackTrace();
-        } catch (IOException ioe) {
+        } catch (
+                IOException ioe) {
             ioe.printStackTrace();
         }
-
+        doc.getDocumentElement().normalize();
+        NodeList nList = doc.getElementsByTagName("sort");
+        Node sortTag = nList.item(0);
+        NodeList childNodes = sortTag.getChildNodes();
+        for (int i = 0; i < childNodes.getLength(); i++) {
+            Node node = childNodes.item(i);
+            System.out.println("Current Element:" + node.getNodeName());
+            if (node.getNodeType() == Node.ELEMENT_NODE) {
+                Element eElement = (Element) node;
+                String tagName = eElement.getTagName();
+                String textContent = eElement.getTextContent();
+                System.out.println("Name:" + tagName + "Tag" + textContent);
+                configMap.put(tagName, textContent);
+            }
+        }
+    return configMap;
     }
 }

dc88ac9f95a8a3045f235d1374054562deeea85f
annbarovskaya <annbarovskaya@gmail.com>
2023-02-28 11:00:03 +0300

    Added changes

--- store/src/main/java/Comparator.java
+++ store/src/main/java/Comparator.java
@@ -1,17 +1,18 @@
 import module3.oop.Product;
 
-public class Comparator implements Comparator<Product> {
+public class ProductComparator implements Comparator<Product> {
     @Override
-    public int compare(Product p1, Product p2)
-    {
-        int nameCompare = p1.getName().compareTo(p2.getName());
-        if (nameCompare !=0) {
-            return nameCompare;
-        }
-        int ratingCompare = Double.compare(p1.getRate(), p2.getRate());
-        if (ratingCompare !=0) {
-            return ratingCompare;
-        }
-        return Double.compare(p1.getPrice(), p2.getPrice());
+    public int comparingByName() {
+        Comparator<Product>productNameComparator = Comparator.comparing(Product::getName, (p1, p2))
+        return p2.compareTo(p1);
     }
+    public int comparingByPrice() {
+        Comparator<Product>productPriceComparator = Comparator.comparing(Product::getPrice, (p1, p2))
+        return p2.compareTo(p1);
+    }
+    public int comparingByRate() {
+        Comparator<Product>productRateComparator = Comparator.comparing(Product::getRate, (p1, p2))
+        return p2.compareTo(p1);
+    }
+
 }
